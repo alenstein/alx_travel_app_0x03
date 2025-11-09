09d83e077dd87d7ee1e2872da0a19fe3cc06c24b
Allen Sibanda <sibandallen@gmail.com>
2025-11-09 22:44:30 +0200

    fix: ModuleNotFoundError: No module named 'alx_travel_app.asgi'

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '0.0.0.0'])
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
--- alx_travel_app/build.sh
+++ alx_travel_app/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
